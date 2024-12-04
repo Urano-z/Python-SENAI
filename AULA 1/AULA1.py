71a4c4a93c42c1e47a69bee3c3a3c15820442eae
Urano-z <andreza.af.trabalhos@gmail.com>
2024-12-04 09:54:19 -0300

    Add aula 1 sobre print

---
+++ AULA 1/AULA1.py
@@ -0,0 +1,4 @@
+print('Olá Mundo!')
+print('Sejam todos bem vindos')
+print('Iniciando hoje a aula de python...')
+print('Mesmo sendo diferente, espero gostar de python, como gostei de programar no ensino médio')
